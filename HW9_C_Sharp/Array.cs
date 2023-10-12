098f09df3413f88ceff192ab6c6ff1ddea9413de
Pavlo <pkhasanov@gmail.com>
2023-10-12 22:26:19 +0300

    Add project files.

---
+++ HW9_C_Sharp/Array.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9_C_Sharp
+{
+    internal class Array<T> : ICalc<T> where T : struct, IComparable<T>
+    {
+        public int Size { get; set; }
+        private T[] _arr;
+        public Array(int size)
+        {
+            _arr = new T[size];
+            Size = size;
+        }
+        public T this[int i]
+        {
+            get { return _arr[i]; }
+            set { _arr[i] = value; }
+        }
+        public static Array<T> operator +(Array<T> a, Array<T> b)
+        {
+            Array<T> result = new Array<T>(a.Size);
+
+            for (int i = 0; i < a.Size; ++i)
+            {
+                result[i] = (dynamic)a[i] + b[i];
+            }
+
+            return result;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Size; ++i)
+            {
+                sb.Append(this[i]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        public int Less(T valueToCompare)
+        {
+            int count = 0;
+            foreach (var item in _arr)
+            {
+                if (item.CompareTo(valueToCompare) < 0)
+                    count++;
+            }
+            return count;
+        }
+
+        public int Greater(T valueToCompare)
+        {
+            int count = 0;
+            foreach (var item in _arr)
+            {
+                if (item.CompareTo(valueToCompare) > 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ HW9_C_Sharp/ICalc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9_C_Sharp
+{
+    internal interface ICalc<T> where T : struct, IComparable<T>
+    {
+        int Less(T valueToCompare);
+        int Greater(T valueToCompare);
+    }
+}
